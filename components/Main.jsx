b5037897d3879e461833892890a9deffcd24c03b
Gian Rafael Palermo <rafael@Rafaels-MacBook-Pro.local>
2022-09-09 10:08:42 +0800

    added skills section, typing animation

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {AiOutlineClose, AiOutlineMail, AiOutlineMenu} from 'react-icons/ai'
 import {FaGithub, FaLinkedinIn} from 'react-icons/fa'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
+import { TypeAnimation } from 'react-type-animation';
 
 const Main = () => {
   return (
@@ -10,7 +11,22 @@ const Main = () => {
             <div>
             <p className='uppercase text-sm tracking-widest text-gray-600'>ayo what am i doing here?</p>
             <h1 className='py-4 text-gray-700'>Hi, I'm <span className='text-[#5651ef]'>Rafael</span></h1>
-            <h1 className='py-4 text-gray-700'>A Full Stack Developer</h1>
+            <h1 className='py-4 text-gray-700'>
+                <TypeAnimation 
+                    sequence={[
+                        'A Software Engineer',
+                        1500,
+                        'A UI/UX Designer',
+                        1500,
+                        'A Tech Enthusiast',
+                        1500,
+                        'A Student Athlete',
+                        1000,
+                    ]}
+                    cursor={true}
+                    repeat={Infinity}
+                />
+            </h1>
             <p className='py-4 text-gray-600 max-w-[70%] m-auto'>awdabdw d ahwo ad adwhoawhd hadwo adohsadoihoh awh aodh</p>
             <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                 <div className='rounded-full shadow-lg shadow-gray-300 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
