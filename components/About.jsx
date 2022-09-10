5597e2359ae231c84d668e05414bcaa4a809f89d
Rafael <ralipalermo@hotmail.com>
2022-09-11 00:20:11 +0800

    testing about profile photo V2

--- components/About.jsx
+++ components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import AboutImg from '../public/assets/profile.jpeg'
 
 const About = () => {
   return (
@@ -16,7 +17,7 @@ const About = () => {
                 <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my projects!</p>
             </div>
             <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 easi-in duration-300'>
-            <Image className='rounded-xl' src='../public/assets/profile.jpeg' alt='/' width='500' height='500' />
+            <Image className='rounded-xl' src={AboutImg} alt='/' width='500' height='500' />
             </div>
         </div>
     </div>
